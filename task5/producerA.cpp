44baaaddeb783d6c900dd2815e37185f559003d2
Md Washik Al Azad <37837507+Washik124034@users.noreply.github.com>
2019-09-11 17:24:45 +0600

    Add files via upload

---
+++ task5/producerA.cpp
@@ -0,0 +1,176 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/**
+ * Copyright (c) 2011-2015  Regents of the University of California.
+ *
+ * This file is part of ndnSIM. See AUTHORS for complete list of ndnSIM authors and
+ * contributors.
+ *
+ * ndnSIM is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * ndnSIM is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * ndnSIM, e.g., in COPYING.md file.  If not, see <http://www.gnu.org/licenses/>.
+ **/
+
+#include "producerA.hpp"
+#include "ns3/log.h"
+#include "ns3/string.h"
+#include "ns3/uinteger.h"
+#include "ns3/packet.h"
+#include "ns3/simulator.h"
+
+#include "model/ndn-l3-protocol.hpp"
+#include "helper/ndn-fib-helper.hpp"
+
+#include <memory>
+
+NS_LOG_COMPONENT_DEFINE("ndn.ProducerA");
+
+namespace ns3 {
+namespace ndn {
+
+NS_OBJECT_ENSURE_REGISTERED(ProducerA);
+
+TypeId
+ProducerA::GetTypeId(void)
+{
+  static TypeId tid =
+    TypeId("ns3::ndn::ProducerA")
+      .SetGroupName("Ndn")
+      .SetParent<App>()
+      .AddConstructor<ProducerA>()
+      .AddAttribute("Prefix", "Prefix, for which producer has the data", StringValue("/"),
+                    MakeNameAccessor(&ProducerA::m_prefix), MakeNameChecker())
+      .AddAttribute(
+         "Postfix",
+         "Postfix that is added to the output data (e.g., for adding producer-uniqueness)",
+         StringValue("/"), MakeNameAccessor(&ProducerA::m_postfix), MakeNameChecker())
+      .AddAttribute("PayloadSize", "Virtual payload size for Content packets", UintegerValue(1024),
+                    MakeUintegerAccessor(&ProducerA::m_virtualPayloadSize),
+                    MakeUintegerChecker<uint32_t>())
+      .AddAttribute("Freshness", "Freshness of data packets, if 0, then unlimited freshness",
+                    TimeValue(Seconds(0)), MakeTimeAccessor(&ProducerA::m_freshness),
+                    MakeTimeChecker())
+      .AddAttribute(
+         "Signature",
+         "Fake signature, 0 valid signature (default), other values application-specific",
+         UintegerValue(0), MakeUintegerAccessor(&ProducerA::m_signature),
+         MakeUintegerChecker<uint32_t>())
+      .AddAttribute("KeyLocator",
+                    "Name to be used for key locator.  If root, then key locator is not used",
+                    NameValue(), MakeNameAccessor(&ProducerA::m_keyLocator), MakeNameChecker());
+  return tid;
+}
+
+ProducerA::ProducerA()
+	: m_sameInterestCount(1)
+	, m_previousInterestName("")
+	, m_isSyncedNeeded(false)
+{
+  NS_LOG_FUNCTION_NOARGS();
+}
+
+// inherited from Application base class.
+void
+ProducerA::StartApplication()
+{
+  NS_LOG_FUNCTION_NOARGS();
+  App::StartApplication();
+
+  FibHelper::AddRoute(GetNode(), m_prefix, m_face, 0);
+}
+
+void
+ProducerA::StopApplication()
+{
+  NS_LOG_FUNCTION_NOARGS();
+
+  App::StopApplication();
+}
+
+void
+ProducerA::OnInterest(shared_ptr<const Interest> interest)
+{
+  App::OnInterest(interest); // tracing inside
+
+  NS_LOG_FUNCTION(this << interest);
+
+  if (!m_active)
+    return;
+
+
+  Name dataName(interest->getName());
+  //auto intendedProducer  = dataName.getSubName(2,1);
+
+  auto data = make_shared<Data>();
+  data->setName(dataName);
+  data->setFreshnessPeriod(::ndn::time::milliseconds(m_freshness.GetMilliSeconds()));
+
+  if(dataName.getSubName(2,1).equals("/sync"))
+  {
+	  std::string payloadString;
+	  if(m_isSyncedNeeded)
+	  {
+		  payloadString = "1";
+		  m_isSyncedNeeded = false;
+	  }
+	  else
+		  payloadString = "0";
+
+	  const char* buff = payloadString.c_str();
+	  size_t buff_size = payloadString.length();
+	  data->setContent(make_shared< ::ndn::Buffer>(buff,buff_size));
+	  //if(m_sameInterestCount == 1)
+	  //	  m_isSyncedNeeded = false;
+  }
+
+  else
+  {
+	  if(m_previousInterestName.equals(""))
+	  	  m_previousInterestName = dataName;
+	  else if(m_previousInterestName.equals(dataName))
+	  	  m_sameInterestCount++;
+	  if(m_sameInterestCount == 3)
+	  {
+	  	  m_isSyncedNeeded = true;
+	  	  m_sameInterestCount = 1;
+	  	  m_previousInterestName = "";
+	  	  return;
+	  }
+
+	  data->setContent(make_shared< ::ndn::Buffer>(m_virtualPayloadSize));
+
+  }
+
+
+
+
+
+  Signature signature;
+  SignatureInfo signatureInfo(static_cast< ::ndn::tlv::SignatureTypeValue>(255));
+
+  if (m_keyLocator.size() > 0) {
+    signatureInfo.setKeyLocator(m_keyLocator);
+  }
+
+  signature.setInfo(signatureInfo);
+  signature.setValue(::ndn::makeNonNegativeIntegerBlock(::ndn::tlv::SignatureValue, m_signature));
+
+  data->setSignature(signature);
+
+  NS_LOG_INFO("node(" << GetNode()->GetId() << ") responding with Data: " << data->getName());
+
+  // to create real wire encoding
+  data->wireEncode();
+
+  m_transmittedDatas(data, this, m_face);
+  m_appLink->onReceiveData(*data);
+}
+
+} // namespace ndn
+} // namespace ns3
